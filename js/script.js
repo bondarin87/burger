60b8a760d428de1399d24afe51ecbcc76cc6be1b
bondarin87 <110008441+bondarin87@users.noreply.github.com>
2024-11-09 15:36:36 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -45,6 +45,19 @@ if (isMobile.any()) {
     document.body.classList.add('_pc');
 }
 
+// menu burger
+
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+    
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    } );
+}
+
 // click -> bajando hasta sierto seccion
 
 const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
@@ -58,6 +71,11 @@ const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
                 const gotoBlock = document.querySelector(menuLink.dataset.goto);
                 const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
 
+                if(iconMenu.classList.contains('_active')) {
+                    document.body.classList.remove('_lock');
+                    iconMenu.classList.remove('_active');
+                    menuBody.classList.remove('_active');
+                }
                     window.scrollTo ({
                        top: gotoBlockValue,
                        behavior: "smooth"
